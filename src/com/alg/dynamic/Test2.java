9bf93e79542c74189bae6e310c6eddbd0348e861
trevor <trevorl@synnex.com>
2020-03-25 22:34:42 +0800

    init

--- src/com/alg/dynamic/Test2.java
+++ src/com/alg/dynamic/Test2.java
@@ -25,7 +25,7 @@ public class Test2 {
         }
         int[][] p = new int[m][n];
         for (int i = 0; i < m ;i++) {
-            for (int j = 0 ; i< n ;j++) {
+            for (int j = 0 ; j< n ;j++) {
                 if (i == 0 || j == 0) {
                     p[i][j] = 0;
                 }else {
