10eafd80876b9b123c29d0481d8b75f6a76cc4c6
trevor <trevorl@synnex.com>
2020-04-08 23:21:35 +0800

    init

---
+++ src/com/alg/tree/TwoTree.java
@@ -0,0 +1,18 @@
+package com.alg.tree;
+
+public class TwoTree<T> {
+
+    private T leftChild;
+
+    private T rightChild;
+
+    private T root;
+
+
+    private static class Node{
+
+
+    }
+
+
+}
